79c09089730f43f58477fae825a02966b10c8716
Lingyu Li <woaililingyu2009@126.com>
2015-06-24 13:40:17 +0800

    macos opencv getting startted

--- hello_opencv/main.cpp
+++ hello_opencv/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv/cvaux.hpp>
+#include <fstream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+#define BYTE unsigned char
+
+int main(int argc, const char * argv[])
+
+{
     // insert code here...
-    std::cout << "Hello, World!\n";
+#if 1
+    //get the image from the directed path
+    IplImage* img = cvLoadImage("/Users/lilingyu1/Documents/sample.jpg", 1);
+    //NSLog(img);
+    //create a window to display the image
+    cvNamedWindow("picture", 1);
+    //show the image in the window
+    cvShowImage("picture", img);
+    //wait for the user to hit a key
+    cvWaitKey(0);
+    //delete the image and window
+    cvReleaseImage(&img);
+    cvDestroyWindow("picture");
+#endif
+    //return
     return 0;
 }
+
