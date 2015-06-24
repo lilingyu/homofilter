7352ebc9f45fd495f3b7fd3e05b2f89198abf379
Lingyu Li <woaililingyu2009@126.com>
2015-06-24 13:26:30 +0800

    Initial Commit

---
+++ hello_opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello_opencv
+//
+//  Created by LiLingyu on 15/6/24.
+//  Copyright (c) 2015年 LiLingyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
